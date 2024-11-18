cdc96bbd67004ed9d2b8fc9a6da867fe69a8c971
hunte <hunter421@ksu.edu>
2024-11-17 20:55:53 -0600

    j

--- 560Proj/TheaterProj/TheaterProj/SQL/Procedures/Movies.GetAllMovies.sql
+++ 560Proj/TheaterProj/TheaterProj/SQL/Procedures/Movies.GetAllMovies.sql
@@ -1,5 +1,7 @@
 ﻿CREATE OR ALTER PROCEDURE Movies.GetAllMovies AS
 
-Select *
-FROM Movies.Movie
+Select M.MovieName, M.ReleaseYear, M.Runtime, M.AverageUserScore, M.GenreTypeID, M.ContentRatingID
+FROM Movies.Movie M
+	INNER JOIN Theaters.ShowTime ST ON M.MovieID = ST.MovieID
+WHERE ST.[DateTime] >= GETDATE()
 GO
